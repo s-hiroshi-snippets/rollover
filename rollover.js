325cef9bbf2defcdecb13f437ccca16cb700a3e9
Sawai Hiroshi <info@info-town.jp>
2013-01-25 14:16:17 +0900

    Improve

---
+++ rollover.js
@@ -0,0 +1,37 @@
+/**
+ * rollover
+ *
+ * HTMLUlElementの子孫要素のロールオーバー機能を提供する
+ *
+ * {@code
+ * <ul>
+ *   <li><img src=""></li>
+ * </ul>
+ * }
+ *
+ * @param {Object} options key/value pair
+ * <li>selector ロールオーバーするセレクター
+ * <li>suffix ロールオーバー画像の接尾辞
+ * <li>画像の拡張子
+ */
+jQuery.fn.rollover = function(options) {
+    options =  options || {
+        selector: 'img',
+        suffix: '_over',
+        extension: 'png'
+
+    };
+    $(options.selector, this).each(function() {
+        var dir;
+        var path;
+        $(this).hover(function() {
+            dir = $(this).attr('src').split('.' + options.extension)[0];
+            path = dir + options.suffix + '.' + options.extension;
+            $(this).attr('src', path);
+        }, function() {
+            dir = $(this).attr('src').split(options.suffix)[0];
+            path = dir + '.' + options.extension;
+            $(this).attr('src', path);
+        });
+    });
+};
